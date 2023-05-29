50a4c8a80734b74d326e20dbcb909a64a234ddfa
aakash19here <spacex3nasa@gmail.com>
2023-05-29 14:24:28 +0530

    feat:project-2

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,22 @@
-import Todo from "./components/Todo";
-import { usePost } from "./lib/use-posts";
+import { useState } from "react";
 
 const App = () => {
-  const { data, isLoading, status } = usePost();
-  const image =
-    "https://cdn.discordapp.com/attachments/1110143249479381012/1111512703010345070/13808884147_cartoon_monsters_downtown_Tokyo_Studio_Ghibli_style_340cc9c3-a20c-49cb-9986-5f711478f951.png";
-
-  if (status === "error") return <div>Oops , Failed to fetch </div>;
-
+  const [color, setColor] = useState<string>("");
   return (
     <>
-      <div>
-        {isLoading ? (
-          "Loading"
-        ) : (
-          <div className="mt-5 laptop:mt-10 grid grid-cols-1 tablet:grid-cols-2 gap-4">
-            {data?.map((item) => (
-              <Todo
-                key={item.id}
-                name={item.title}
-                description={item.body}
-                img={image}
-              />
-            ))}
-          </div>
-        )}
+      <div className="h-screen flex flex-col justify-center items-center">
+        <div
+          className={`w-[330px] h-[330px] border border-black flex justify-center`}
+          style={{ backgroundColor: color }}
+        >
+          {color === "" ? "Empty" : color}
+        </div>
+        <input
+          placeholder="Enter your a color"
+          value={color}
+          onChange={(e) => setColor(e.target.value)}
+          className="border border-black"
+        />
       </div>
     </>
   );
